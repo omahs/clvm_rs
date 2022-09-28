42cb236b5de31385b0ecd4c739fbc5795d5d7f88
Richard Kiss <him@richardkiss.com>
2022-09-28 13:52:09 -0700

    Some speed improvements.

--- wheel/src/adapt_response.rs
+++ wheel/src/adapt_response.rs
@@ -13,13 +13,13 @@ pub fn adapt_response(
 ) -> PyResult<(PyObject, LazyNode)> {
     match response {
         Ok(reduction) => {
-            let val = LazyNode::new(Rc::new(allocator), reduction.1);
+            let val = LazyNode::new(py, Rc::new(allocator), reduction.1);
             let rv: PyObject = reduction.0.into_py(py);
             Ok((rv, val))
         }
         Err(eval_err) => {
             let rv: PyObject = eval_err.1.into_py(py);
-            let val = LazyNode::new(Rc::new(allocator), eval_err.0);
+            let val = LazyNode::new(py, Rc::new(allocator), eval_err.0);
             Ok((rv, val))
         }
     }
